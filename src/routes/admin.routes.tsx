c8af984e2f4460edfb60efff1e543c2b6b3ba8d9
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-02-18 23:45:38 +0600

    added dynamic sidebar items

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -3,10 +3,17 @@ import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
+import { NavLink } from "react-router-dom";
 
 type TRoute = {
   path: string;
   element: ReactNode;
+};
+
+type TSidebarItem = {
+  key: string;
+  label: ReactNode;
+  children?: TSidebarItem[];
 };
 
 const adminPaths2 = [
@@ -36,6 +43,30 @@ const adminPaths2 = [
     ],
   },
 ];
+
+export const adminSidebarItems = adminPaths2.reduce(
+  (acc: TSidebarItem[], item) => {
+    if (item.path && item.name) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+      });
+    }
+    if (item.children) {
+      acc.push({
+        key: item.name,
+        label: item.name,
+        children: item.children.map((child) => ({
+          key: child.name,
+          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+        })),
+      });
+    }
+
+    return acc;
+  },
+  []
+);
 
 export const adminRoutes = adminPaths2.reduce((acc: TRoute[], item) => {
   if (item.path && item.element) {
