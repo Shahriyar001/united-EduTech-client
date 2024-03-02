e7a50ad3eb38e948ec8dad833bb192072921ad23
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-03-02 23:59:38 +0600

    added react hook form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  return <div></div>;
+  const { register, handleSubmit } = useForm;
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text" name="" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="text" name="" id="password" {...register("password")} />
+      </div>
+      <Button type="submit">Login</Button>
+    </form>
+  );
 };
 
 export default Login;
